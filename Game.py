6667ff946afb0409f6e594885710a04d0a22b32f
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-23 16:35:43 -0400

    initiate pygame and python3

---
+++ Game.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+s = pygame.display.set_mode((400, 300))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
