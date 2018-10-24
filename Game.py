6697783a4a52ac5cd89d11eebb9e525568f12826
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-24 10:27:54 -0400

    create objects for board

--- Game.py
+++ Game.py
@@ -1,17 +1,26 @@
 import pygame as pg
 
 pg.init()
-screen = pg.display.set_mode((400, 300))
+sw = 700
+sh = 700
+screen = pg.display.set_mode((sw, sh))
 done = False
 blue = 0
 clock = pg.time.Clock()
-fps = 30
+fps = 60
+
+# Build the board
+# goal = new Goal()
 
 while not done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
+
+    ############################################ Fill Code
     screen.fill((0, 0, blue))
     blue += 1
+    ############################################ Fill Code
+
     pg.display.flip()
     clock.tick(fps)
