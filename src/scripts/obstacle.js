1a6281452f0a705820c483371f3e12fac6220094
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-12-23 22:42:55 -0500

    width max change

--- src/scripts/obstacle.js
+++ src/scripts/obstacle.js
@@ -1,6 +1,6 @@
 class Obstacle{
     constructor(){
-        this.w = random(10, windowWidth - 200)
+        this.w = random(10, windowWidth/2)
         this.h = 20
         this.pos = createVector(random(20, windowWidth-this.w-20), random(goal.pos.y+20, windowHeight-footOffset-200))
     }
